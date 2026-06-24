02bafdd2288d85423496a80774563f62537ac34d
diego <diegovbmachado@gmail.com>
2026-06-24 18:56:21 -0300

    atualizando navbar esquerda

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,10 +1,46 @@
 import { Sheet, SheetTrigger, SheetContent} from "@/components/ui/sheet"
 import { Button } from "../ui/button"
 import Link from "next/link"
-import { PanelBottom } from "lucide-react"
+import { Home, Package, PanelBottom, Settings2, ShoppingBag, User } from "lucide-react"
+import { TooltipProvider, TooltipTrigger, Tooltip, TooltipContent } from "../ui/tooltip"
 export function Sidebar() {
   return (
     <div className="flex w-full flex-col bg-muted/40">
+
+
+<aside 
+className="fixed inset-y-0 lefet-0 z-10 hidden w-14 border-r bg-background sm:flex" 
+>
+<nav className="flex flex-col items-center gap-4 px-2 py-5"> 
+     <TooltipProvider>
+        <Link
+        href="#"
+        className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full"
+        >
+            <Package className="h-4 w-4"/>
+            <span className="sr-only">Dashboard Avatar</span>
+        </Link>
+
+        <Tooltip>
+            <TooltipTrigger asChild>
+            <Link
+        href="#"
+        className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+        >
+            <Home className="h-5 w-5"/>
+            <span className="sr-only">Início</span>
+        </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+                <p>Início</p>
+            </TooltipContent>
+        </Tooltip>
+        </TooltipProvider>
+
+
+</nav>
+</aside>
+    
     <div className="sm:hidden flex flex-col sm:gap-4 sm:pl-14">
 <header className="sticky tiop-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-b-0 sm:bg-transparent sm:px-6">
 <Sheet>
@@ -14,14 +50,63 @@ export function Sidebar() {
 <span className="sr-only">Abrir / fechar menu</span>
         </Button>
         </SheetTrigger>
-        <SheetContent>
-            <nav>
-               <Link href="#">
+        <SheetContent side="left" className="sm:max-w-x">
+            <nav className="grid gap-6 text-lg font-medium">
+               <Link 
+               href="#"
+               className="flex h-10 w-10 bg-primary rounded-full text-lg items-center justify-center text-primary-foreground md:text-base gap-2"
+               prefetch={false}
+               >
+                <Package className="h-5 w-5 transition-all"/>  
                <span>Logo</span>
                </Link>
+
+                <Link 
+               href="#"
+               className="felx items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+               prefetch={false}
+               >
+                <Home className="h-5 w-5 transition-all"/>  
+               Início
+               </Link>
+
+                <Link 
+               href="#"
+               className="felx items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+               prefetch={false}
+               >
+                <ShoppingBag className="h-5 w-5 transition-all"/>  
+               Pedidos
+               </Link>
+               <Link 
+               href="#"
+               className="felx items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+               prefetch={false}
+               >
+                <Package className="h-5 w-5 transition-all"/>  
+               Produtos
+               </Link>
+               <Link 
+               href="#"
+               className="felx items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+               prefetch={false}
+               >
+                <User className="h-5 w-5 transition-all"/>  
+               Clientes
+               </Link>
+               <Link 
+               href="#"
+               className="felx items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+               prefetch={false}
+               >
+                <Settings2 className="h-5 w-5 transition-all"/>  
+               Configurações
+               </Link>
+
             </nav>
         </SheetContent>
 </Sheet>
+<h2>Menu</h2>
 </header>
     </div>
     </div>
